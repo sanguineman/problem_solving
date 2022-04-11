f0e4b94da914a566220e7c2307e7caf4f08afbd6
sanguineman <72648309+sanguineman@users.noreply.github.com>
2022-04-11 21:39:52 +0900

    Update 7562.cpp
    Refactoring

--- 백준/BFS/7562.cpp
+++ 백준/BFS/7562.cpp
@@ -7,8 +7,8 @@ const int INF = 5e5;
 int T, I;
 int dx[8] = {-2,-1,1,2,2,1,-1,-2};
 int dy[8] = {1,2,2,1,-1,-2,-2,-1};
-bool visited[301][301];
-queue <pair<pair<int,int>,int>> Q;
+int dist[301][301]; // 굳이 bool visited[301][301]을 쓸 필요없이 dist 배열로 거리와 방문 체크를 동시에 진행 가능
+queue <pair<int,int>> Q;
 int main() { 	
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
 	cin >> T;
@@ -17,32 +17,24 @@ int main() {
 		pair <int,int> now, after;
 		cin >> now.X >> now.Y;
 		cin >> after.X >> after.Y;
-		Q.push({{now.X,now.Y},0});
-		visited[now.X][now.Y] = 1;
-		int mn = INF;
+		for(int i = 0; i < I; i++)
+			fill(dist[i],dist[i]+I,-1);
+		Q.push({now.X,now.Y});
+		dist[now.X][now.Y] = 0;
 		while(!Q.empty()){
 			auto cur = Q.front();
 			Q.pop();
-			if(cur.X.X == after.X && cur.X.Y == after.Y){
-				if(cur.Y < mn)
-					mn = cur.Y;
-			}
 			for(int dir = 0; dir < 8; dir++){
-				int nx = cur.X.X + dx[dir];
-				int ny = cur.X.Y + dy[dir];
+				int nx = cur.X + dx[dir];
+				int ny = cur.Y + dy[dir];
 				if(nx < 0 || nx >= I || ny < 0 || ny >= I)
 					continue;
-				if(visited[nx][ny])
+				if(dist[nx][ny] >= 0)
 					continue;
-				Q.push({{nx,ny},cur.Y + 1});
-				visited[nx][ny] = 1;
+				Q.push({nx,ny});
+				dist[nx][ny] = dist[cur.X][cur.Y] + 1;
 			}
 		}
-		cout << mn << '\n';
-		for(int i = 0; i < I; i++){
-			for(int j = 0; j < I; j++){
-				visited[i][j] = 0;
-			}
-		}
+		cout << dist[after.X][after.Y] << '\n';
 	}
 }
