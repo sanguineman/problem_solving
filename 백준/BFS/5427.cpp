369906270267d24c4f024ce49a57d4c48d01d52c
sanguineman <72648309+sanguineman@users.noreply.github.com>
2022-04-13 02:02:06 +0900

    Update 5427.cpp
    최초 탈출 시점이 가장 최소 시간이 되기 때문에 매번 값을 비교하며 갱신할 필요가 없다.

--- 백준/BFS/5427.cpp
+++ 백준/BFS/5427.cpp
@@ -1,80 +1,67 @@
-#include <bits/stdc++.h>  
-using namespace std; 	
+#include <bits/stdc++.h> 
+using namespace std; 
 using ll = long long;  
-const int INF = 5e5;  
+const int LMT = 1e3;
 #define X first
 #define Y second
-char board[1001][1001];
-int w, h, T;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
-bool visited[1001][1001];
-queue <pair<pair<int,int>,int>> Q;
+int T, w, h;
+char board[LMT+1][LMT+1];
+int dist[LMT+1][LMT+1];
+queue <pair<int,int>> Q;
 int main() { 	
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> T;
 	while(T--){
 		cin >> w >> h;
-		for(int i = 0; i < h; i++){
-			string st;
-			cin >> st;
-			for(int j = 0; j < w; j++){
-				board[i][j] = st[j];
-			}
-		}
-		pair <int,int> start;
+		for(int i = 0; i < h; i++)
+			fill(dist[i],dist[i]+w,-1);
+		pair <int,int> s;
 		for(int i = 0; i < h; i++){
 			for(int j = 0; j < w; j++){
+				cin >> board[i][j];
 				if(board[i][j] == '*'){
-					Q.push({{i,j},-1});
-					visited[i][j] = 1;
+					Q.push({i,j});
 				}
 				else if(board[i][j] == '@'){
-					start.X = i;
-					start.Y = j;
-					visited[i][j] = 1;
+					s.X = i; 
+					s.Y = j;
 				}
 			}
 		}
-		int mn = INT_MAX;
-		Q.push({{start.X,start.Y},1});
+		Q.push({s.X,s.Y});
+		dist[s.X][s.Y] = 0;
+		bool check = false;
 		while(!Q.empty()){
-			auto cur = Q.front();
-			Q.pop();
-			if(board[cur.X.X][cur.X.Y] == '@' && (cur.X.X == 0 || cur.X.X == h - 1 || cur.X.Y == 0 || cur.X.Y == w - 1)){
-				if(cur.Y < mn)
-					mn = cur.Y;
-				continue;
-			}
+			auto cur = Q.front(); Q.pop();
+			if(board[cur.X][cur.Y] == '@' && (cur.X == 0 || cur.X == h-1 || cur.Y == 0 || cur.Y == w-1)){
+				cout << dist[cur.X][cur.Y] + 1 << '\n';
+				check = true;
+				break;
+			} 
 			for(int dir = 0; dir < 4; dir++){
-				int nx = cur.X.X + dx[dir];
-				int ny = cur.X.Y + dy[dir];
+				int nx = cur.X + dx[dir];
+				int ny = cur.Y + dy[dir];
 				if(nx < 0 || nx >= h || ny < 0 || ny >= w)
 					continue;
-				if(visited[nx][ny] || board[nx][ny] == '#')
+				if(dist[nx][ny] >= 0 || board[nx][ny] == '*' || board[nx][ny] == '#')
 					continue;
-				if(board[cur.X.X][cur.X.Y] == '@'){
-					if(board[nx][ny] == '*')
-						continue;
-					Q.push({{nx,ny},cur.Y+1});
-					board[nx][ny] = '@';
-					visited[nx][ny] = 1;
-				}
-				else if(board[cur.X.X][cur.X.Y] == '*'){
-					Q.push({{nx,ny},-1});
+				if(board[cur.X][cur.Y] == '*'){
+					Q.push({nx,ny});
 					board[nx][ny] = '*';
-					visited[nx][ny] = 1;
+				}
+				else if(board[cur.X][cur.Y] == '@'){
+					board[nx][ny] = '@';
+					dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+					Q.push({nx,ny});
 				}
 			}
 		}
-		if(mn == INT_MAX)
+		if(!check)
 			cout << "IMPOSSIBLE" << '\n';
-		else
-			cout << mn << '\n';
-		for(int i = 0; i < h; i++){
-			for(int j = 0; j < w; j++){
-				visited[i][j] = 0;
-			}
+		while(!Q.empty()){
+			Q.pop();
 		}
 	}
 }
